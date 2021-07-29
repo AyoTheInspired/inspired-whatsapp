1ff8bc67714cc98da2aa6736259af41737bf7a17
Ayo <bimboladxtreme@gmail.com>
2021-07-29 01:21:44 +0100

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDhDY4NHhgzp2vELKMmHKF7X2u5DT0019Y",
+	authDomain: "whatsapp-clone-5becd.firebaseapp.com",
+	projectId: "whatsapp-clone-5becd",
+	storageBucket: "whatsapp-clone-5becd.appspot.com",
+	messagingSenderId: "113361870754",
+	appId: "1:113361870754:web:950850e80a26e649619cfb",
+	measurementId: "G-0SD0CWJ2PE",
+};
