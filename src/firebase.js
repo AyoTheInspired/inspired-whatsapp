9b8457dc1175b87b1e42bf84cf2a023fc2c868c9
Ayo <bimboladxtreme@gmail.com>
2021-07-29 14:00:53 +0100

    Added firebase functionality

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
 	apiKey: "AIzaSyDhDY4NHhgzp2vELKMmHKF7X2u5DT0019Y",
 	authDomain: "whatsapp-clone-5becd.firebaseapp.com",
@@ -7,3 +9,15 @@ const firebaseConfig = {
 	appId: "1:113361870754:web:950850e80a26e649619cfb",
 	measurementId: "G-0SD0CWJ2PE",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+
+export default db;
