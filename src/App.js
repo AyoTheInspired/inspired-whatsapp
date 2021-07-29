789fe409fe86b6dc41eaa698a7afdff91c70a79d
Ayo <bimboladxtreme@gmail.com>
2021-07-29 01:38:35 +0100

    Added start page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
 	return (
 		<div className="App">
-			HELLO WORLD
-			<h3>I'M THE WHATSAPP CLONE</h3>
-			<h3>AND YOU CAN ALWAYS REACH ME HERE</h3>
+			<div className="app__body">app body</div>
 		</div>
 	);
 }
