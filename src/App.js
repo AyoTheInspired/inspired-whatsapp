67d9b2d95e9ae7ac45fe75d62db38c98e2fe2384
Ayo <bimboladxtreme@gmail.com>
2021-07-29 03:48:23 +0100

    Added chat screen header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 
 function App() {
 	return (
 		<div className="App">
 			<div className="app__body">
 				<Sidebar />
+				<Chat />
 			</div>
 		</div>
 	);
