61bbf32a8205f861288dfd46d0f87fb07dffa4eb
Ayo <bimboladxtreme@gmail.com>
2021-07-29 13:40:24 +0100

    Setup and styled chat footer

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,15 +3,24 @@ import { Avatar, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 import "./Chat.css";
 
 function Chat() {
+	const [input, setInput] = useState("");
 	const [seed, setSeed] = useState("");
 
 	useEffect(() => {
 		setSeed(Math.floor(Math.random() * 5000));
 	}, []);
+
+	const sendMessage = (e) => {
+		e.preventDefault();
+
+		setInput("");
+	};
 
 	return (
 		<div className="chat">
@@ -37,13 +46,28 @@ function Chat() {
 				</div>
 			</div>
 			<div className="chat__body">
+				{input}
 				<p className={`chat__message ${true && "chat__receiver"}`}>
 					<span className="chat__name">Ayo Abimbola</span>
 					Hey Guys
 					<span className="chat__timestamp">12:30pm</span>
 				</p>
 			</div>
-			<div className="chat__footer"></div>
+			<div className="chat__footer">
+				<InsertEmoticonIcon />
+				<form>
+					<input
+						type="text"
+						placeholder="Type a message"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+					/>
+					<button type="submit" onClick={sendMessage}>
+						Send a Message
+					</button>
+				</form>
+				<MicIcon />
+			</div>
 		</div>
 	);
 }
