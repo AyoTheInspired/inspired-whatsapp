696a9b0c75ec834b79b3792855e978e4042530b0
Ayo <bimboladxtreme@gmail.com>
2021-07-29 21:27:32 +0100

    Built the login component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import Login from "./components/Login";
 
 function App() {
+	const [user, setUser] = useState(null);
+
 	return (
 		<div className="App">
-			<div className="app__body">
-				<Router>
-					<Sidebar />
-					<Switch>
-						<Route path="/rooms/:roomId">
-							<Chat />
-						</Route>
-						<Route path="/">
-							<Chat />
-						</Route>
-					</Switch>
-				</Router>
-			</div>
+			{!user ? (
+				<Login />
+			) : (
+				<div className="app__body">
+					<Router>
+						<Sidebar />
+						<Switch>
+							<Route path="/rooms/:roomId">
+								<Chat />
+							</Route>
+							<Route path="/">
+								<Chat />
+							</Route>
+						</Switch>
+					</Router>
+				</div>
+			)}
 		</div>
 	);
 }
